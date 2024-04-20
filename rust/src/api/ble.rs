2f76004355f555f09f7b4d4d93418845244bad16
weixian.zhou <weixian.zhou@ninebot.com>
2024-04-21 00:15:17 +0800

    rs jni pending

--- rust/src/api/ble.rs
+++ rust/src/api/ble.rs
@@ -2,9 +2,13 @@ use std::collections::HashMap;
 use std::sync::{Arc, OnceLock};
 
 use anyhow::Result;
-use btleplug::api::{bleuuid::BleUuid, Central, CentralEvent, Manager as _, ScanFilter};
+use btleplug::api::{Central, CentralEvent, Manager as _, ScanFilter};
 pub use btleplug::platform::Manager;
 use futures::stream::StreamExt;
+use jni::objects::{JClass, JObject, JString, JValue};
+use jni::sys::{jint, jobjectArray};
+use jni::JNIEnv;
+use tokio::runtime::Runtime;
 use tokio::{
     sync::{mpsc, Mutex},
     time,
@@ -88,7 +92,9 @@ pub fn init() -> Result<()> {
         while let Some(msg) = rx.recv().await {
             match msg {
                 Command::Scan { sink, filter } => {
-                    tokio::spawn(async { inner_scan(sink, filter).await.unwrap() });
+                    tokio::spawn(async { inner_scan(filter, move |devices| {
+                        sink.add(devices);
+                    }).await.unwrap() });
                 }
                 Command::Connect { id } => inner_connect(id).await.unwrap(),
                 Command::Disconnect { id } => inner_disconnect(id).await.unwrap(),
@@ -149,8 +155,10 @@ async fn send_devices(sink: &StreamSink<Vec<BleDevice>>) -> Result<()> {
 pub fn scan(sink: StreamSink<Vec<BleDevice>>, filter: Vec<String>) -> Result<()> {
     send(Command::Scan { sink, filter })
 }
-
-async fn inner_scan(sink: StreamSink<Vec<BleDevice>>, _filter: Vec<String>) -> Result<()> {
+async fn inner_scan<F>(_filter: Vec<String>, sink_callback: F) -> Result<()>
+where
+    F: Fn(Vec<BleDevice>) + Send + 'static,
+{
     let manager = Manager::new().await?;
     let adapters = manager.adapters().await?;
     let central = adapters.into_iter().next().expect("cannot fail");
@@ -166,16 +174,21 @@ async fn inner_scan(sink: StreamSink<Vec<BleDevice>>, _filter: Vec<String>) -> R
         services: _filter.iter().map(|s| Uuid::parse_str(s).unwrap()).collect(),
     })
     .await?;
-
+    trace!("ZWX start_scan {:?}", _filter);
+    let mut cnt = 0;
     let mut device_send_interval = time::interval(time::Duration::from_secs(1));
     while get_is_scanning() {
         tokio::select! {
             _ = device_send_interval.tick() => {
                 // 超30s没更新的设备，从设备列表中移除
                 remove_stale_devices(30).await;
+                cnt += 1;
+                if cnt > 10 {
+                    trace!("ZWX stop scan");
+                    set_is_scanning(false);
+                }
             }
             Some(event) = events.next() => {
-                // tracing::debug!(format!("{:?}", event));
                 match event {
                     CentralEvent::DeviceDiscovered(id) => {
                         tracing::debug!("{}",format!("DeviceDiscovered: {:?}", &id));
@@ -185,12 +198,11 @@ async fn inner_scan(sink: StreamSink<Vec<BleDevice>>, _filter: Vec<String>) -> R
                         devices.insert(id.to_string(), peripheral);
                         // 释放锁
                         drop(devices);
-                        // 发现设备立即发送设备列表
-                        if send_devices(&sink).await.is_err() {
-                            tracing::debug!("{}",format!("DeviceDiscovered send_devices ERR"));
-                            break;
-                        }
-                        tracing::debug!("{}",format!("==> thread: {:?}", std::thread::current().name()));
+                        // 发现设备立即调用闭包将设备添加到给定的 StreamSink
+                        let devices = DEVICES.get().unwrap().lock().await;
+                        let devices = devices.values().map(|d| BleDevice::from_peripheral(d)).collect::<Vec<_>>();
+                        let devices = futures::future::join_all(devices).await;
+                        sink_callback(devices);
                     }
                     CentralEvent::DeviceUpdated(id) => {
                         let mut devices = DEVICES.get().unwrap().lock().await;
@@ -215,28 +227,13 @@ async fn inner_scan(sink: StreamSink<Vec<BleDevice>>, _filter: Vec<String>) -> R
                     CentralEvent::ManufacturerDataAdvertisement {
                         id:_,
                         manufacturer_data:_,
-                    } => {
-                        // tracing::debug!("{}",format!(
-                        //     "ManufacturerDataAdvertisement: {:?}, {:?}",
-                        //     id, manufacturer_data
-                        // ));
-                    }
-                    CentralEvent::ServiceDataAdvertisement { id:_, service_data:_ } => {
-                        // tracing::debug!("{}",format!(
-                        //     "ServiceDataAdvertisement: {:?}, {:?}",
-                        //     id, service_data
-                        // ));
-                    }
-                    CentralEvent::ServicesAdvertisement { id:_, services } => {
-                        let _services: Vec<String> =
-                            services.into_iter().map(|s| s.to_short_string()).collect();
-                        // tracing::debug!("{}",format!("ServicesAdvertisement: {:?}, {:?}", id, services));
-                    }
+                    } => {}
+                    CentralEvent::ServiceDataAdvertisement { id:_, service_data:_ } => {}
+                    CentralEvent::ServicesAdvertisement { id:_, services } => {}
                 }
             }
         }
     }
-    // tracing::debug!("Scan finished");
     Ok(())
 }
 
@@ -279,4 +276,119 @@ async fn inner_stop_scan() -> Result<()> {
     let central = adapters.into_iter().next().expect("cannot fail");
     central.stop_scan().await?;
     Ok(())
+}
+
+
+// 包装异步 Rust 函数，使其能够从 C++ 中调用
+// frb:skip
+#[no_mangle]
+pub extern "C" fn scan_wrapper<F>(filter: Vec<String>, sink_callback: F) 
+where
+    F: Fn(Vec<BleDevice>) + Send + 'static,
+{
+    // 将回调函数转换为 Rust 的闭包
+    let cb = move |devices: Vec<BleDevice>| {
+        // 将 devices 转换为 C++ 中的字符串数组并调用回调函数
+        sink_callback(devices);
+    };
+
+    // 调用异步 Rust 函数
+    inner_scan(filter, cb);
+}
+
+// 声明 init_wrapper 函数
+// frb:skip
+#[no_mangle]
+pub extern "C" fn init_wrapper() -> i32 {
+    match init() {
+        Ok(_) => 0,
+        Err(_) => 1,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn rust_add(n1: i32, n2: i32) -> i32 {
+    n1 + n2
+}
+
+use log::{trace, LevelFilter};
+use android_logger::Config;
+
+fn native_activity_create() {
+    android_logger::init_once(
+        Config::default().with_max_level(LevelFilter::Trace),
+    );
+}
+
+#[no_mangle]
+pub extern "C" fn Java_com_example_bleandroid_RustBridge_initWrapper(env: JNIEnv, _class: JClass) -> i32 {
+    trace!("ZWX init ----------------------------------------------------------------");
+    init_wrapper()
+}
+
+#[no_mangle]
+pub extern "C" fn Java_com_example_bleandroid_RustBridge_add(env: JNIEnv, _class: JClass, n1: jint, n2: jint) -> jint {
+    native_activity_create();
+    trace!("ZWX add ----------------------------------------------------------------");
+    rust_add(n1, n2) * 2
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_example_bleandroid_RustBridge_scanWrapper(
+    env: JNIEnv,
+    _: JObject, // jobject thiz
+    filter: jobjectArray,
+    callback: JObject,
+) {
+    trace!("ZWX scan ----------------------------------------------------------------");
+    // Convert jobjectArray to Vec<JObject>
+    let filter_vec: Vec<String> = (0..env.get_array_length(filter).unwrap()).map(|i| {
+        let item: Result<JString<'_>, jni::errors::Error> = env.get_object_array_element(filter, i as i32).map_or_else(|e| Err(e), |obj| Ok(JString::from(obj)));
+        let item_str = match env.get_string(item.unwrap()) {
+        Ok(s) => s.into(),
+        Err(e) => {
+            // Handle the error
+            panic!("Failed to convert jstring to String: {}", e);
+        }
+        };
+        item_str
+    }).collect();
+
+    let jvm = env.get_java_vm().unwrap();
+
+    // 将回调函数转换为 Rust 的闭包
+    let cb = move |devices: Vec<BleDevice>| {
+        trace!("ZWX cb ----------------------------------------------------------------");
+        let env = jvm.attach_current_thread().unwrap();
+        trace!("ZWX cb1 ----------------------------------------------------------------");
+
+        // Convert Vec<BleDevice> to Array<BleDevice>
+        let array_class = env.find_class("com/example/bleandroid/BleDevice").unwrap();
+        let array = env.new_object_array(devices.len() as i32, array_class, JObject::null()).unwrap();
+        for (i, device) in devices.iter().enumerate() {
+            let java_device = env.new_object(
+                "com/example/bleandroid/BleDevice",
+                "(Ljava/lang/String;Ljava/lang/String;)V",
+                &[JValue::Object(env.new_string(&device.id).unwrap().into()), JValue::Object(env.new_string(&device.name).unwrap().into())],
+            ).unwrap();
+            env.set_object_array_element(array, i as i32, java_device).unwrap();
+        }
+
+        // call callback
+
+        // // Call the Kotlin callback method
+        // let method_name = "onDeviceFound";
+        // let method_signature = "([com/example/bleandroid/BleDevice;)V";
+        // env.call_method(callback, method_name, method_signature, &[JValue::Object(array.into())])
+        //     .expect("Failed to call callback method");
+    };
+    let future = async { 
+            trace!("ZWX spawn ----------------------------------------------------------------");
+            inner_scan(filter_vec, move |devices| {
+                trace!("{}",format!("DeviceDiscovered: {:?}", devices));
+            }).await.unwrap();
+        };
+        let runtime = Runtime::new().unwrap();
+        runtime.block_on(future);
+        trace!("ZWX return");
 }
